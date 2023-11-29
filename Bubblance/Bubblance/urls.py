52f0a71e4e9ed01649cc6f44e758acc0a303de55
rix2009 <rix1928@gmail.com>
2023-11-29 22:20:28 +0200

    user login register and logout
    create module BUser

--- Bubblance/Bubblance/urls.py
+++ Bubblance/Bubblance/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BubblanceApp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
+
 ]
--- Bubblance/BubblanceApp/models.py
+++ Bubblance/BubblanceApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+
+class BUser(User):
+    class UserType(models.IntegerChoices):
+        Driver = 1
+        Manager = 2
+        Client = 3
+    
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    israeliid = models.CharField(max_length=9, validators=[MinLengthValidator(9)], unique=True)
+    phonenumber = models.CharField(max_length=10)
+    usertype = models.IntegerField(choices=UserType.choices)
+    rememberme = models.BooleanField(default=False)
+    image = models.ImageField(default=None)
+
