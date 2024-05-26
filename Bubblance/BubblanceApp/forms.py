3b2ac03c739357a8b1c47ef84e7f8371e94a1eca
rix2009 <rix1928@gmail.com>
2024-05-26 18:45:32 +0300

    update

--- Bubblance/BubblanceApp/forms.py
+++ Bubblance/BubblanceApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from BubblanceApp.models import BUser, Ambulance, EqInAmbulance, AmbulanceCrew, CustomerRequest
+from BubblanceApp.models import BUser, Ambulance, EqInAmbulance, AmbulanceCrew, CustomerRequest, Customer, Institution
 from django.forms import ModelForm
 
 
@@ -84,6 +84,63 @@ class EqupmentInAmbulanceForm(ModelForm):
 		return eq
 	
 
-# class CustomerRequestForm(ModelForm):
+class CustomerRequestForm(ModelForm):
+	class Meta:
+		model = CustomerRequest
+		fields = ("customer_id","pick_from_institution", "pickup_institution", "pick_up_location",
+			"num_of_floors", "elvator_in_home",
+			"drop_at_institution","dropoff_institution","drop_of_location",
+			"pick_up_time","return_trip", "return_trip_pick_up_time",
+			"two_stuff_needed", "need_oxygen", "need_stretcher", "need_wheel_chair",
+			"have_preferred_driver",  "preferred_driver")
+	
+	def __init__(self, *args, **kwargs):
+		super(CustomerRequestForm, self).__init__(*args, **kwargs)
+		self.fields['pick_from_institution'].label = 'Pick-up from an Instiitution?'
+		self.fields['pickup_institution'].label = 'Choose a pick-up Instiitution'
+		self.fields['pick_up_location'].label = 'Pick-up Address'
+		self.fields['num_of_floors'].label = 'Number of floors at home'
+		self.fields['elvator_in_home'].label = 'Home with Elevator?'
+		self.fields['drop_at_institution'].label = 'Drop-off at an Instiitution?'
+		self.fields['dropoff_institution'].label = 'Choose a drop-off Instiitution'
+		self.fields['drop_of_location'].label = 'Drop-off Address'
+		self.fields['pick_up_time'].label = 'Pick-up date and time'
+		self.fields['return_trip'].label = 'Return trip needed?'
+		self.fields['return_trip_pick_up_time'].label = 'Return trip pick-up date and time'
+		self.fields['two_stuff_needed'].label = 'Need more than one stuff member?'
+		self.fields['need_oxygen'].label = 'Need Oxygen?'
+		self.fields['need_stretcher'].label = 'Need Stretcher?'
+		self.fields['need_wheel_chair'].label = 'Need Wheel chair?'
+		self.fields['have_preferred_driver'].label = 'Have a favorite driver?'
+		self.fields['preferred_driver'].label = 'Pick a driver'
+
+	def save(self, commit=True):
+		req = super(CustomerRequestForm, self).save(commit=False)
+		if commit:
+			req.save()
+		return req
 
 
+class NewCustomer(ModelForm):
+	class Meta:
+		model = Customer
+		fields = ("patient_name","contact_name", "contact_phone",
+			"customer_type", "institution_id")
+
+	def save(self, commit=True):
+		customer = super(NewCustomer, self).save(commit=False)
+		if commit:
+			customer.save()
+		return customer
+
+
+class NewInstitution(ModelForm):
+	class Meta:
+		model = Institution
+		fields = ("institution_name","institution_adress", "in_institution")
+
+	def save(self, commit=True):
+		institution = super(NewInstitution, self).save(commit=False)
+		if commit:
+			institution.save()
+		return institution
